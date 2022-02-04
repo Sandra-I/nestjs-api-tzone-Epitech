52908b90061e4ee4ff178c3613f9e0a3def12258
Florent Mouton <flormouton@gmail.com>
2022-02-04 11:57:58 +0100

    init pipeline

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigurationService } from './configuration/configuration/configuration.service';
 
+/**
+ * Set the used port in the constructor
+ */
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigurationService],
 })
-export class AppModule {}
+export class AppModule {
+  static port: number | string;
+
+  /**
+   * @param configurationService - [readonly] Used to retrieve the port number
+   */
+  constructor(private readonly configurationService: ConfigurationService) {
+    AppModule.port = this.configurationService.port;
+  }
+}
