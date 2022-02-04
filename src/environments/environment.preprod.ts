100f832209e19794b7d68862605e24115d24d187
Florent Mouton <flormouton@gmail.com>
2022-02-04 15:31:24 +0100

    edit: preprod environment

--- src/environments/environment.preprod.ts
+++ src/environments/environment.preprod.ts
@@ -1,9 +1,9 @@
 const credentials = {
-    DB_CONN_USER: "tzone_user",
-    DB_CONN_PASSWORD: "6e09f35e336fcc8ee312aa15f35e773a5680eebe239d89068d86eb958fa8a695",
+    DB_CONN_USER: "TZone-user-preprod",
+    DB_CONN_PASSWORD: "9d8e4520e29a59b7016400b6ba131816a5373fc94e3503d602908f21d5487f87",
 }
 
 export default {
     DB_CONN_STRING: `mongodb+srv://${credentials.DB_CONN_USER}:${credentials.DB_CONN_PASSWORD}@yamikamisama.fr:27018`,
-    DB_NAME: "TZone-dev"
+    DB_NAME: "TZone-Preprod"
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 const credentials = {
-    DB_CONN_USER: "tzone_user",
+    DB_CONN_USER: "TZone-user",
     DB_CONN_PASSWORD: "6e09f35e336fcc8ee312aa15f35e773a5680eebe239d89068d86eb958fa8a695",
 }
 
